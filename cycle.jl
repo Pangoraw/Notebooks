7c9e691763b55ce2801687d3961b8dfe221adef8
Paul <paul@plutojl.org>
2022-05-24 21:10:45 +0200

    add cycle notebook

---
+++ cycle.jl
@@ -0,0 +1,36 @@
+### A Pluto.jl notebook ###
+# v0.19.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5b44a0ee-48cb-4bb2-9c39-ac1a960f47a8
+y = z
+
+# ╔═╡ d20187f7-b4c0-40cc-a9e2-2a0e36c77290
+z = x
+
+# ╔═╡ 40a0ecbc-db92-11ec-1797-854b3ed9c08f
+x = y
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═40a0ecbc-db92-11ec-1797-854b3ed9c08f
+# ╠═5b44a0ee-48cb-4bb2-9c39-ac1a960f47a8
+# ╠═d20187f7-b4c0-40cc-a9e2-2a0e36c77290
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
