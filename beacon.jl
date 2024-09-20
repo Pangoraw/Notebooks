2abdfb61ef8f57ed24f359d0f7ef65579213e7e7
Paul Berg <paul.berg@univ-ubs.fr>
2024-09-20 11:16:06 +0200

    beacon cover letter

---
+++ beacon.jl
@@ -0,0 +1,49 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 16e598d0-ed31-4ee1-9717-a8dd83e07794
+md"""
+**Paul Berg** \
+[paul@ber.gp](mailto:paul@ber.gp)
+"""
+
+# ╔═╡ 59fa350a-fa98-4065-9f45-64d736206d20
+html"""
+<div style="float: right; text-align: right;">
+	<b>Team @ Beacon Biosignals</b>
+	<br>
+	<a href="https://beacon.bio">beacon.bio</a>
+</div>
+"""
+
+# ╔═╡ 12839526-772d-11ef-0a4c-6347aebd5b34
+md"""
+###### Cover letter for MLOps engineer position.
+
+$(html"<br />")
+
+Dear Mr/Mrs,
+
+I am excited to apply for the MLOps engineer position at Beacon Biosignals. I think it would be an great career opportunity to join the team. Consequently, I think my skills and interests could be a great match with the company culture.
+
+Indeed, I am currently finishing my PhD in which I had to design and train deep learning systems for vision related tasks. This made me experience the challenges in working with such systems. For example, I had to design and perform annotation tooling for expert annotations tasks such as marine mammal detection. These years in the vision research community also gave me an appreciation for statistics, machine learning and optimization.
+
+In my free time, I am also involved in the Julia community. First, through my contributions to the Pluto.jl editor for which I have been a core contributor since 2020. I like developping developer tooling and I am not scared to dive deep in the stack. This has lead me to contribute through many place of the stack, from the frontend code editor through which I am typing these lines to the allocation optimization pass in the Julia codebase.
+
+Joining a company such as Beacon Biosignals would be an exciting opportunity to contribute to solving such an important problem that is neurological diseases.
+
+I look forward to hearing from you,
+
+Sincerely,
+
+*Paul*\
+*20th September 2024*
+"""
+
+# ╔═╡ Cell order:
+# ╟─16e598d0-ed31-4ee1-9717-a8dd83e07794
+# ╟─59fa350a-fa98-4065-9f45-64d736206d20
+# ╟─12839526-772d-11ef-0a4c-6347aebd5b34
